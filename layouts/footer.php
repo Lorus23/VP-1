d9796d0dfca4be422f3ae1491e83852add2e7396
sayat23 <sayat23@mail.ru>
2018-06-11 16:08:37 +0600

    first correction

---
+++ layouts/footer.php
@@ -0,0 +1,38 @@
+<section class="section map">
+    <div class="yandex-map" id="map"></div>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__address">Санкт-Петербург, ул.Бабушкина, д.12/1, 15</div><a class="footer__phone" href="tel:+78123771377">+7 (812) 377-13-77</a>
+        </div>
+    </footer>
+</section>
+</div>
+</div>
+
+<div class="full-review popup" id="full-review"><a class="full-review__close" href="#"><svg id="icon" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
+            <path fill-rule="evenodd" clip-rule="evenodd" fill="#E45028"
+                  d="M9.831 7.494l4.673-4.673A1.651 1.651 0 1 0 12.167.484L7.494 5.157 2.82.483A1.653 1.653 0 0 0 .483 2.82l4.673 4.673-4.673 4.673a1.653 1.653 0 0 0 2.337 2.337L7.493 9.83l4.673 4.673c.646.646 1.691.646 2.337 0s.646-1.691 0-2.337L9.831 7.494z"/>
+        </svg></a>
+    <div class="full-review__inner">
+        <div class="full-review__title">Констнтин Спилберг</div>
+        <div class="full-review__content">
+            Мысли все о них и о них, о них и о них. Нельзя устоять, невозможно забыть... Никогда не думал, что булочки могут быть такими мягкими, котлетка такой сочной, а сыр таким расплавленным. Мысли все о них и о них, о них и о них. Нельзя устоять, невозможно забыть... Никогда не думал, что булочки могут быть такими мягкими, котлетка такой сочной, а сыр таким расплавленным.
+
+        </div>
+    </div>
+</div>
+<div class="status-popup popup" id="success">
+    <div class="status-popup__inner">
+        <div class="status-popup__message">Сообщение отправлено</div><a class="status-popup__close btn" href="#">Закрыть</a>
+    </div>
+</div>
+<div class="status-popup popup" id="error">
+    <div class="status-popup__inner">
+        <div class="status-popup__message error-message">Произошла ошибка</div><a class="status-popup__close btn" href="#">Закрыть</a>
+    </div>
+</div>
+<script src="./js/vendors.min.js"></script>
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+<script src="./js/main.min.js"></script>
+</body>
+</html>
