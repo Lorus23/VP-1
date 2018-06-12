58b501c47507b04807cf9e12429e59cbe3c9033a
sayat23 <sayat23@mail.ru>
2018-06-12 11:07:47 +0600

    реализована запись заказа в БД часть 2 и запись заказа от существующего пользователя

--- function.php
+++ function.php
@@ -14,123 +14,78 @@ $payment2 = $_POST['payment2'];
 $callback = $_POST['callback'];
 $array1 = $_POST;
 
-//
-//function checkEmailExists($email)
-//{
-//    // Соединение с БД
-//    $db = Db::getConnection();
-//
-//    // Текст запроса к БД
-//    $sql = 'SELECT * FROM users WHERE email = :email';
-//
-//    // Получение результатов. Используется подготовленный запрос
-//    $result = $db->prepare($sql);
-//    $result->bindParam(':email', $email, PDO::PARAM_STR);
-//    $result->setFetchMode(PDO::FETCH_ASSOC);
-//    $result->execute();
-//    return $result->fetch();
-//}
-//
-//function register($email, $phone, $name)
-//{
-//    // Соединение с БД
-//    $db = Db::getConnection();
-//
-//    // Текст запроса к БД
-//    $sql = 'INSERT INTO users (username, email, phone) VALUES (:name, :email, :phone)';
-//
-//    // Получение и возврат результатов. Используется подготовленный запрос
-//    $result = $db->prepare($sql);
-//    $result->bindParam(':name', $name, PDO::PARAM_STR);
-//    $result->bindParam(':email', $email, PDO::PARAM_STR);
-//    $result->bindParam(':phone', $phone, PDO::PARAM_STR);
-//    return $result->execute();
-//}
-//
-//function zakaz($id, $array)
-//{
-//
-//    // Соединение с БД
-//    $db = Db::getConnection();
-//
-//    // Текст запроса к БД
-//    $sql = "INSERT INTO `order_burger` (`user_id`, `street`, `home`, `part`, `appt`, `floor`, `comment`, `payment1`,`payment2`, `callback`) VALUES (:user_id, :street, :home, :part, :appt, :floor, :comment, :payment1, :payment2, :callback)";
-//    $result = $db->prepare($sql);
-//    // Получение и возврат результатов. Используется подготовленный запрос
-//    $values = (object)$array;
-//
-//
-//    $result->bindParam(':user_id', $id);
-//    $result->bindParam(':street', $values->street);
-//    $result->bindParam(':home', $values->home);
-//    $result->bindParam(':part', $values->part);
-//    $result->bindParam(':appt', $values->appt);
-//    $result->bindParam(':floor', $values->floor);
-//    $result->bindParam(':comment', $values->comment);
-//    $result->bindParam(':payment1', $values->payment1);
-//    $result->bindParam(':payment2', $values->payment2);
-//    $result->bindParam(':callback', $values->callback);
-//
-//    return $result->execute();
-//}
-//
-//if (checkEmailExists($email)) {
-//var_dump($result);
-//    zakaz($result['id'], $array1);
-//
-//
-//} else {
-//    register($email, $phone, $name);
-//    zakaz($result['id'], $array1);
-//}
-//
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+function checkEmailExists($email)
+{
+    // Соединение с БД
+    $db = Db::getConnection();
 
-//MySQL connection details.
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$database = 'burgers';
+    // Текст запроса к БД
+    $sql = 'SELECT * FROM users WHERE email = :email';
 
-//Custom PDO options.
-$options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => false
-);
-
-//Connect to MySQL and instantiate our PDO object.
-$pdo = new PDO("mysql:host=$host;dbname=$database", $user, $pass, $options);
-$pdo->exec("SET CHARACTER SET utf8");
-$id = '1';
-//Create our INSERT SQL query.
-$sql = "INSERT INTO `order_burger` (`user_id`, `street`, `home`, `part`, `appt`, `floor`, `comment`, `payment1`,`payment2`, `callback`) VALUES (:user_id, :street, :home, :part, :appt, :floor, :comment, :payment1, :payment2, :callback)";
-
-////Prepare our statement.
-$result = $pdo->prepare($sql);
-// Получение и возврат результатов. Используется подготовленный запрос
-$values = (object)$array1;
-
-
-$result->bindParam(':user_id', $id);
-$result->bindParam(':street', $values->street);
-$result->bindParam(':home', $values->home);
-$result->bindParam(':part', $values->part);
-$result->bindParam(':appt', $values->appt);
-$result->bindParam(':floor', $values->floor);
-$result->bindParam(':comment', $values->comment);
-$result->bindParam(':payment1', $values->payment1);
-$result->bindParam(':payment2', $values->payment2);
-$result->bindParam(':callback', $values->callback);
-
-//Execute the statement and insert our values.
-$inserted = $result->execute();
-
-if($inserted){
-    echo 'Row inserted!';
+    // Получение результатов. Используется подготовленный запрос
+    $result = $db->prepare($sql);
+    $result->bindParam(':email', $email, PDO::PARAM_STR);
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+    $result->execute();
+    $id = $result->fetch();
+    if ($id) {
+        return $id['id'];} else {
+        return false;}
 }
 
-$id = $pdo->lastInsertId();
+function register($email, $phone, $name)
+{
+    // Соединение с БД
+    $db = Db::getConnection();
 
-var_dump($id);
+    // Текст запроса к БД
+    $sql = 'INSERT INTO users (username, email, phone) VALUES (:name, :email, :phone)';
+
+    // Получение и возврат результатов. Используется подготовленный запрос
+    $result = $db->prepare($sql);
+    $result->bindParam(':name', $name, PDO::PARAM_STR);
+    $result->bindParam(':email', $email, PDO::PARAM_STR);
+    $result->bindParam(':phone', $phone, PDO::PARAM_STR);
+
+    //Execute the statement and insert our values.
+    $inserted = $result->execute();
+
+    if ($inserted) {
+        echo 'Row inserted!';
+    }
+    return $id = $db->lastInsertId();
+}
+
+function zakaz($array1, $id)
+{
+
+    // Соединение с БД
+    $db = Db::getConnection();
+
+    // Текст запроса к БД
+    $sql = "INSERT INTO `order_burger` (`user_id`, `street`, `home`, `part`, `appt`, `floor`, `comment`, `payment1`,`payment2`, `callback`) VALUES (:user_id, :street, :home, :part, :appt, :floor, :comment, :payment1, :payment2, :callback)";
+    $result = $db->prepare($sql);
+
+    // Получение и возврат результатов. Используется подготовленный запрос
+    $values = (object)$array1;
+    $result->bindParam(':user_id', $id);
+    $result->bindParam(':street', $values->street);
+    $result->bindParam(':home', $values->home);
+    $result->bindParam(':part', $values->part);
+    $result->bindParam(':appt', $values->appt);
+    $result->bindParam(':floor', $values->floor);
+    $result->bindParam(':comment', $values->comment);
+    $result->bindParam(':payment1', $values->payment1);
+    $result->bindParam(':payment2', $values->payment2);
+    $result->bindParam(':callback', $values->callback);
+    return $result->execute();
+}
+if (checkEmailExists($email)) {
+    $id = checkEmailExists($email);
+    zakaz($array1,$id);
+} else {
+    $id = register($email, $phone, $name);
+    zakaz($array1, $id);
+}
+
